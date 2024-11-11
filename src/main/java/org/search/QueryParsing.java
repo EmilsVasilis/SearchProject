2e6ffd40b1e57a90af13e64394bf06e3b5eb2acb
Indigo Bosworth <indigobosworth@gmail.com>
2024-11-11 18:17:14 +0000

    Updated naive implementation to boost queries

--- src/main/java/org/search/QueryParsing.java
+++ src/main/java/org/search/QueryParsing.java
@@ -17,6 +17,7 @@ import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.BoostQuery;
 
 public class QueryParsing {
     private static final String TOPIC_DIRECTORY = "src/main/resources/topics";
@@ -25,6 +26,11 @@ public class QueryParsing {
     private static final String TITLE_HEAD = "(<title>)";
     private static final String DESCRIPTION_HEAD = "(<desc> Description: )";
     private static final String NARRATIVE_HEAD = "(<narr> Narrative: )";
+
+    private static final float TITLE_BOOST = 4.0f;
+    private static final float DESCRIPTION_BOOST = 1.7f;
+    private static final float NARRATIVE_BOOST = 1.2f;
+    private static final float IRRELEVANT_BOOST = 2.0f;
 
     // Parse topics file, breaking up by topic, then return a query per topic
     public List<Query> parseTopicsFile(Analyzer analyzer) throws IOException {
@@ -79,21 +85,30 @@ public class QueryParsing {
         String narrative = matcher.group(2);
 
         // pass collected information to analyzer for pre-processing
-        // TODO: Narrative and Number are ignored temporarily
-        List<String> titleTerms = analyzeTextToTerms(title, analyzer);
-        List<String> descriptionTerms = analyzeTextToTerms(title, analyzer);
 
-        // Add all terms to query
-        // TODO: Most Naive approach is taken for now, all terms added as SHOULD
+        //layout of how to generate queries
         BooleanQuery.Builder query = new BooleanQuery.Builder();
+        // Generate a query from the title field with a boost of 4.0
+        List<String> titleTerms = analyzeTextToTerms(title, analyzer);
         for(String term : titleTerms){
             Query qterm = new TermQuery(new Term("title", term));
-        	query.add(new BooleanClause(qterm, BooleanClause.Occur.SHOULD));
+            Query boostTerm = new BoostQuery(qterm, TITLE_BOOST);
+        	query.add(new BooleanClause(boostTerm, BooleanClause.Occur.SHOULD));
         }
+
+        // Generate a query from the description with a boost of 1.7
+        List<String> descriptionTerms = analyzeTextToTerms(title, analyzer);
         for(String term : descriptionTerms){
             Query qterm = new TermQuery(new Term("description", term));
-        	query.add(new BooleanClause(qterm, BooleanClause.Occur.SHOULD));
+            Query boostTerm = new BoostQuery(qterm, DESCRIPTION_BOOST);
+        	query.add(new BooleanClause(boostTerm, BooleanClause.Occur.SHOULD));
         }
+
+        // Run a method on the narrative splitting it into Relevant and Not Relevant
+
+        // (if present) Generate a query from relevant narrative with boost of 1.2
+
+        // (if present) Generate a filter clause with 2.0 boost for Not Relevant narrative
         
         // Build and return query
         return query.build();
