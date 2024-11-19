51694349cc7fb88ab867cf5f16e6648315dd12d5
Indigo Bosworth <indigobosworth@gmail.com>
2024-11-19 11:51:49 +0000

    updated open mode of index writer

--- src/main/java/org/search/Indexing.java
+++ src/main/java/org/search/Indexing.java
@@ -1,38 +1,21 @@
 
 package org.search;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.core.StopFilter;
-import org.apache.lucene.analysis.core.LowerCaseFilter;
-import org.apache.lucene.analysis.standard.StandardTokenizer;
-import org.apache.lucene.analysis.en.PorterStemFilter;
 import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.document.StringField;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Paths;
 
 public class Indexing {
 
@@ -56,6 +39,7 @@ public class Indexing {
 
         Directory dir = FSDirectory.open(Paths.get("target/index"));  
         IndexWriterConfig config = new IndexWriterConfig(new CustomAnalyzer());  
+        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
         writer = new IndexWriter(dir, config);  
 
         for (Document doc: docs){
